ca5d24a81d7c747c657574d5457f743a550e8a61
Loïc PÉRON <loic.peron@gmail.com>
2023-09-17 16:15:05 +0200

    linux: separate test scripts

---
+++ ubuntu-prepare.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# https://learn.microsoft.com/en-us/powershell/scripting/install/install-ubuntu
+
+# Update the list of packages
+sudo apt-get update
+# Install pre-requisite packages.
+sudo apt-get install -y wget apt-transport-https software-properties-common
+# Download the Microsoft repository GPG keys
+wget -q "https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb"
+# Register the Microsoft repository GPG keys
+sudo dpkg -i packages-microsoft-prod.deb
+# Delete the the Microsoft repository GPG keys file
+rm packages-microsoft-prod.deb
+# Update the list of packages after we added packages.microsoft.com
+sudo apt-get update
+# Install PowerShell
+sudo apt-get install -y powershell
+# Start PowerShell
+# pwsh
+
+# sudo apt upgrade
+sudo apt install -y wine
